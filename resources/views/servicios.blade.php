8bc4ebe29f6c4e30edda46d9c9030f028be9755b
francisco-7240 <franciscojavierriveraarias17@gmail.com>
2026-08-20 16:59:10 -0500

    Create servicios.blade.php
    creacion de pagina servicios

---
+++ resources/views/servicios.blade.php
@@ -0,0 +1,6 @@
+@include('partials.header')
+@include('partials.navbar')
+<div>
+    <!-- I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison -->
+</div>
+@include('partials.footer')
