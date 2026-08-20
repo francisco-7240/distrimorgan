9022d006f2b89269fca243bc615f2093bf4e204d
francisco-7240 <franciscojavierriveraarias17@gmail.com>
2026-08-20 16:55:27 -0500

    Create contacto.blade.php
    creacion contacto

---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,9 @@
+
+@include('partials.navbar')
+<head>
+    @include('partials.header')
+    <title>Distribuidora Morgan</title>
+    <meta name="description" content="Distribuidora líder en herramientas y equipos industriales. Más de 15 años ofreciendo calidad y servicio excepcional">
+</head>
+
+@include('partials.footer')
